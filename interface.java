f6136ceb5df655362c7eab62f4ae6d47b98b3086
Daniel Ho <danielho54@gmail.com>
2017-04-01 05:36:16 -0700

    import updates

--- interface.java
+++ interface.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Backend {
     ArrayList<String> links = new ArrayList<>();
     
---
+++ public/main.js
@@ -0,0 +1,1 @@
+window.java.print('Hello Java!');
