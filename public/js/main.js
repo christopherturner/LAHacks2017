6556190f2522a97f6dfc983f81b9db9233d64984
Daniel Ho <danielho54@gmail.com>
2017-04-01 21:54:36 -0700

    injecting images and text

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,15 @@
 //article has img(), text(), and link();
 //array has to be sorted
 var topArticles = [];
-var articles = fetchData();
+//2d array of articles
+var articles;
 
 $(document).ready(function() {
   $("#searchButton").on("click", search);
   $(".card").on("click", function(event){
     expandArticle(this);
   });
+  articles = fetchData();
 });
 
 function search(){
@@ -18,12 +20,11 @@ function search(){
   sort(inputText);
   $("#headline-img").attr("src", topArticles[0].urlToImage);
   $("#headline-text").html(topArticles[0].description);
-  //side story 1
-  $("#side1-img").attr("src", topArticles[1].urlToImage);
-  $("#side1-text").html(topArticles[1].description);
-  //side story 2
-  $("#side2-img").attr("src", topArticles[2].urlToImage);
-  $("#side2-text").html(topArticles[2].description);
+
+  for(var i=1; i<=11; i++){
+    $("#side" + i + "-img").attr("src", topArticles[i].urlToImage);
+    $("#side" + i + "-text").html(topArticles[i].description);
+  }
 }
 
 function expandArticle(obj){
@@ -41,14 +42,20 @@ function expandArticle(obj){
 
 
 //manages article.html Parameter obj is article type
+//
 function updateArticle(obj){
 
 }
 
 //2d array sort PUTS into top 3
+// TODO: input is what the user searches
+// sort the 2d array "articles" into the 1d array topArticles. 
+// The function search() will take in the top 11 entries in topArticles and output them into the html page.
+
 function sort(input){
   //mock code
-  for(var i=0; i<3; i++){
+  for(var i=0; i<10; i++){
     topArticles.push(articles[0][i]);
   }
+  topArticles.push(articles[1][0]);
 }
---
+++ public/readme.md
@@ -0,0 +1,5 @@
+TODO:  
+* make website more responsive, scalable
+* implement the sort function
+* do article page
+* have it run search on startup. Issue: since articles = fetchData(); articles has to be populated before running search();
