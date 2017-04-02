91ddfc68b118b51134ae1b7b08cbb4e353e128bf
Brandon Dalton <daltonb@usc.edu>
2017-04-02 10:42:09 -0700

    Update news.js

--- public/js/news.js
+++ public/js/news.js
@@ -1,28 +1,4 @@
-function fetchDataTest() {
-    var temp = [];
-    $.getJSON(
-        "https://newsapi.org/v1/articles?source=cnn&apiKey=31cd62ebfe8a4a7c92702cea80681433",
-        function (data) {
-        	var obj = JSON.parse(JSON.stringify(data)).articles;
-        	var titles = "";
-        	for (var i = 0; i < obj.length; i++) {
-        		titles += obj[i].title + ", ";
-        	}
-            temp.push(obj);
-        });
-    $.getJSON(
-        "https://newsapi.org/v1/articles?source=bloomberg&apiKey=31cd62ebfe8a4a7c92702cea80681433",
-        function (data) {
-            var obj = JSON.parse(JSON.stringify(data)).articles;
-            var titles = "";
-            for (var i = 0; i < obj.length; i++) {
-                titles += obj[i].title + ", ";
-            }
-            temp.push(obj);
-        });
-    return temp;
-};
-
+var tstory = [];
 function fetchData(){
     var source = ["bbc-news", "bloomberg", "buzzfeed", "cnn", "google-news", "reddit-r-all",
     "the-new-york-times", "the-verge", "national-geographic"];
@@ -35,7 +11,45 @@ function fetchData(){
                 function (data) {
                     var obj = JSON.parse(JSON.stringify(data)).articles;
                     stories.push(obj);
+                    tstory.push(obj);
                 });
     }
     return stories;
 }
+
+function search(item){
+    //tstory = [];
+    item = item.toLowerCase();
+    var sp = item.split(" ");
+    topArticles = [];
+    var temp = fetchData();
+    console.log(sp);
+    
+    for(var i = 0; i < sp.length; i++){
+        console.log(sp[i]);
+        console.log("tstory length: " + tstory.length);
+        for(var h = 0; h < tstory.length; h++){
+            for(var j = 0; j < tstory[h].length; j++){
+                if(tstory[h][i].description.toLowerCase().includes(sp[i])){
+                    console.log("poop");
+                    console.log(tstory[h][i].title);
+                    topArticles.push(tstory[h][j]);
+                 }
+            }
+        }
+    }
+    tstory = [];
+}
+
+/*
+function storyExists(s, found){
+    for(var i = 0; i < found.length; i++){
+        for(var j=0; j< found[i].length; j++){
+            if(found[i][j].title == s.title){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+*/
